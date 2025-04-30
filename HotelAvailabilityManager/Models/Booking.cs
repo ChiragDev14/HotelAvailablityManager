38fb3d7f67f4a79550bb9b89ed4b89630ee8c8b0
Chirag Patel <patelchirag_14@yahoo.com>
2025-04-30 21:33:12 +0530

    Add files via upload
    dotnet run -- --hotels hotels.json --bookings bookings.json

---
+++ HotelAvailabilityManager/Models/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HotelAvailabilityManager.Models
+{
+    public class Booking
+    {
+        [JsonPropertyName("hotelId")]
+        public string HotelId { get; set; }
+        [JsonPropertyName("arrival")]
+        public string Arrival { get; set; }
+        [JsonPropertyName("departure")]
+        public string Departure { get; set; }
+        [JsonPropertyName("roomType")]
+        public string RoomType { get; set; }
+        [JsonPropertyName("roomRate")]
+        public string RoomRate { get; set; }
+    }
+
+}
