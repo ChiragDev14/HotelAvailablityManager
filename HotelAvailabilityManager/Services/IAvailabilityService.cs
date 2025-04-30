b23c8af0043be842e38f70446f3134bb0164c53c
Chirag Patel <patelchirag_14@yahoo.com>
2025-04-30 21:38:37 +0530

    dotnet run -- --hotels hotels.json --bookings bookings.json
    dotnet run -- --hotels hotels.json --bookings bookings.json

---
+++ HotelAvailabilityManager/Services/IAvailabilityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAvailabilityManager.Services
+{
+    public interface IAvailabilityService
+    {
+        int GetAvailability(string hotelId, string roomType, DateTime start, DateTime end);
+        List<string> SearchAvailability(string hotelId, int daysAhead, string roomType, DateTime today);
+    }
+}
